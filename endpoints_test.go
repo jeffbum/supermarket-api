e45579319ad6d24fbdb5bc6f104434d791773808
Jeff Bumgardner <jeff.bumgardner@travasecurity.com>
2022-01-05 10:12:15 -0500

    add missing endpoint unit tests

--- endpoints_test.go
+++ endpoints_test.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/gorilla/mux"
 )
 
 func TestGetProduce(t *testing.T) {
@@ -19,34 +23,95 @@ func TestGetProduce(t *testing.T) {
 			status, http.StatusOK)
 	}
 
-	expected := `[{"produceCode":"A7Fc-VgWD-7m5S-zb9Z","name":"Apple","unitPrice":"$1.23"},{"produceCode":"IgGJ-hHGW-QrWe-sIGY","name":"Orange","unitPrice":"$2.45"}]`
+	expected := `[{"produceCode":"l6m9-5p3n-y5qr-lhel","name":"Apple","unitPrice":"$1.23"},{"produceCode":"yr7k-b6ku-sruk-mnd6","name":"Orange","unitPrice":"$2.45"}]`
 	if rr.Body.String() != expected {
 		t.Errorf("handler returned unexpected body: got %v want %v",
 			rr.Body.String(), expected)
 	}
 }
 
-// func TestGetProduceById(t *testing.T) {
+func TestGetProduceById(t *testing.T) {
 
-// 	req, err := http.NewRequest("GET", "/produce", nil)
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
-// 	q := req.URL.Query()
-// 	q.Add("id", "1")
-// 	req.URL.RawQuery = q.Encode()
-// 	rr := httptest.NewRecorder()
-// 	handler := http.HandlerFunc(getOne)
-// 	handler.ServeHTTP(rr, req)
-// 	if status := rr.Code; status != http.StatusOK {
-// 		t.Errorf("handler returned wrong status code: got %v want %v",
-// 			status, http.StatusOK)
-// 	}
+	req, err := http.NewRequest("GET", "api/v1/produce", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	vars := map[string]string{"produceCode": "l6m9-5p3n-y5qr-lhel"}
+	req = mux.SetURLVars(req, vars)
 
-// 	// Check the response body is what we expect.
-// 	expected := `{"id":1,"first_name":"Krish","last_name":"Bhanushali","email_address":"krishsb2405@gmail.com","phone_number":"0987654321"}`
-// 	if rr.Body.String() != expected {
-// 		t.Errorf("handler returned unexpected body: got %v want %v",
-// 			rr.Body.String(), expected)
-// 	}
-// }
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(getOne)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	// Check the response body is what we expect.
+	expected := `{"produceCode":"l6m9-5p3n-y5qr-lhel","name":"Apple","unitPrice":"$1.23"}`
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
+}
+
+func TestGetProduceByIdNotFound(t *testing.T) {
+	req, err := http.NewRequest("GET", "/produce", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	vars := map[string]string{"produceCode": "l6m9-5p3n-y5qr-lhe"}
+
+	req = mux.SetURLVars(req, vars)
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(getOne)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status == http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusBadRequest)
+	}
+}
+
+
+func TestDeleteEntry(t *testing.T) {
+	req, err := http.NewRequest("DELETE", "/produce", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	vars := map[string]string{"produceCode": "l6m9-5p3n-y5qr-lhel"}
+
+	req = mux.SetURLVars(req, vars)
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(delete)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusNoContent {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusNoContent)
+	}
+}
+
+func TestCreateEntry(t *testing.T) {
+
+	var jsonStr = []byte(`{"name":"Oatmeal","unitPrice":"$2.59"}`)
+
+	req, err := http.NewRequest("POST", "/produce", bytes.NewBuffer(jsonStr))
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(post)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusCreated {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusCreated)
+	}
+	var produce Produce
+	json.NewDecoder(rr.Body).Decode(&produce)
+
+	if produce.Name != "Oatmeal" || produce.UnitPrice != "$2.59" {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), produce)
+	}
+}
